c9c87a6bbc318cd0f17fb71fdd73697a74d4d3ee
unknown <mail@robingenz.de>
2020-10-04 15:50:53 +0200

    feat(login): implement login module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,12 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
+    path: 'login',
+    loadChildren: () => import('./modules/login').then( m => m.LoginPageModule)
+  },
+  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
 ];
---
+++ src/app/modules/login/index.ts
@@ -0,0 +1,4 @@
+export * from './login-routing.module';
+export * from './login.module';
+export * from './pages';
+
