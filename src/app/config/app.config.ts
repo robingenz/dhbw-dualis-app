e4bd43c3fc55320c627d40f63eaee9156f7224a6
Robin Genz <mail@robingenz.de>
2020-10-11 12:11:24 +0200

    add app config

---
+++ src/app/config/app.config.ts
@@ -0,0 +1,5 @@
+export class Config {
+  static readonly dualisBaseUrl: string = 'https://dualis.dhbw.de';
+  static readonly dualisTokenExpirationTimeMs: number = 1740000;
+  static readonly httpTimeout: number = 5;
+}
