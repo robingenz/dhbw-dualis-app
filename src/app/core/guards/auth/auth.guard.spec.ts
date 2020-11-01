a38b910112d7325dd5551815933a52593937f522
Robin Genz <mail@robingenz.dev>
2020-11-01 18:14:29 +0100

    test: add guard tests

--- src/app/core/guards/auth/auth.guard.spec.ts
+++ src/app/core/guards/auth/auth.guard.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
+import { AuthenticationService } from '@app/core/authentication';
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
+  let authServiceSpy: jasmine.SpyObj<AuthenticationService>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    authServiceSpy = jasmine.createSpyObj('AuthenticationService', ['getSession']);
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: AuthenticationService, useValue: authServiceSpy },
+        { provide: Router, useValue: routerSpy },
+      ],
+    });
     guard = TestBed.inject(AuthGuard);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('should not activate the route', async () => {
+    authServiceSpy.getSession.and.returnValue(null);
+    const canActivate = guard.canActivate();
+    expect(canActivate).toBeFalsy();
+  });
+
+  it('should activate the route', async () => {
+    authServiceSpy.getSession.and.returnValue({ key: '123', expirationTimestamp: 123 });
+    const canActivate = guard.canActivate();
+    expect(canActivate).toBeTruthy();
   });
 });
