a8d1c7f8817995c8ca2a039557efb6c4e54d1341
Robin Genz <mail@robingenz.de>
2020-11-06 16:50:44 +0100

    wip

--- scripts/bump-native-versions.js
+++ scripts/bump-native-versions.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-var pjson = require(`../package.json`);
+const pjson = require('../package.json');
 
 function fetchVersion() {
   return pjson.version;
@@ -8,28 +8,49 @@ function fetchVersion() {
 
 function replaceAndroidVersion(version) {
   const buildGradleFilePath = path.join('android', 'app', 'build.gradle');
+  let buildGradleFileContent = readFileContent(buildGradleFilePath);
 
-  const versionCode = ;
+  const versionCodeRegex = /versionCode \d+/;
+  const oldVersionCode = buildGradleFileContent.match(versionCodeRegex)[0].replace('versionCode ', '');
+  const newVersionCode = parseInt(oldVersionCode) + 1;
+  buildGradleFileContent = buildGradleFileContent.replace(versionCodeRegex, `versionCode ${newVersionCode}`);
+
+  const versionNameRegex = /versionName "\d+.\d+.\d+"/;
+  buildGradleFileContent = buildGradleFileContent.replace(versionNameRegex, `versionName "${version}"`);
+
+  writeFileContent(buildGradleFilePath, buildGradleFileContent);
 }
 
 function replaceIosVersion(version) {
   const infoPlistFilePath = path.join('ios', 'App', 'App', 'Info.plist');
+  let infoPlistFileContent = readFileContent(infoPlistFilePath);
 
-  const cfBundleVersion = ;
-  replaceInFile(infoPlistFilePath, );
+  const cfBundleVersionRegex = /<key>CFBundleVersion<\/key>[\n\r]\s+<string>\d+<\/string>/;
+  const oldCFBundleVersion = infoPlistFileContent
+    .match(cfBundleVersionRegex)[0]
+    .replace(/<key>CFBundleVersion<\/key>[\n\r]\s+<string>/, '')
+    .replace(/<\/string>/, '');
+  const newCFBundleVersion = parseInt(oldCFBundleVersion) + 1;
+  infoPlistFileContent = infoPlistFileContent.replace(
+    cfBundleVersionRegex,
+    `<key>CFBundleVersion</key>\n  <string>${newCFBundleVersion}</string>`,
+  );
+
+  const cdBundleShortVersionStringRegex = /<key>CFBundleShortVersionString<\/key>[\n\r]\s+<string>\d+.\d+.\d+<\/string>/;
+  infoPlistFileContent = infoPlistFileContent.replace(
+    cdBundleShortVersionStringRegex,
+    `<key>CFBundleShortVersionString</key>\n  <string>${version}</string>`,
+  );
+
+  writeFileContent(infoPlistFilePath, infoPlistFileContent);
 }
 
-function replaceInFile(file, oldValue, newValue) {
-  const encoding = 'utf8';
-  var content = fs.readFileSync(file, encoding);
-  var matchArr = content.match(oldValue);
-  if (!matchArr) {
-      return;
-  }
-  for (const match of matchArr) {
-      content = content.replace(match, newValue);
-  }
-  fs.writeFileSync(file, content, encoding);
+function readFileContent(filePath, encoding = 'utf8') {
+  return fs.readFileSync(filePath, encoding);
+}
+
+function writeFileContent(filePath, content, encoding = 'utf8') {
+  fs.writeFileSync(filePath, content, encoding);
 }
 
 try {
