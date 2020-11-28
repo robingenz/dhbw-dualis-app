19e27ccc66cfde50978268292da4cbfc13267740
Robin Genz <mail@robingenz.dev>
2020-11-29 00:45:16 +0100

    test: add tests

--- src/app/modules/exam-results/pipes/unit-filter/unit-filter.pipe.spec.ts
+++ src/app/modules/exam-results/pipes/unit-filter/unit-filter.pipe.spec.ts
@@ -1,3 +1,4 @@
+import { Unit } from '../../interfaces';
 import { UnitFilterPipe } from './unit-filter.pipe';
 
 describe('UnitFilterPipe', () => {
@@ -9,5 +10,27 @@ describe('UnitFilterPipe', () => {
 
   it('create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transform correctly', () => {
+    const units: Unit[] = [
+      {
+        id: '111222111222111',
+        displayName: 'Wissenschaftliches Arbeiten',
+        no: 'WWI_100',
+        status: 'bestanden',
+        credits: '5,0',
+        finalGrade: 'b',
+        exams: undefined,
+      },
+    ];
+    let val = pipe.transform(units, 'bestanden');
+    expect(val.length).toBe(1);
+    val = pipe.transform(units, 'Wissen');
+    expect(val.length).toBe(1);
+    val = pipe.transform(units, '100');
+    expect(val.length).toBe(1);
+    val = pipe.transform(units, '4,0');
+    expect(val.length).toBe(0);
   });
 });
