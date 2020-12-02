f4704c5d5c3014ee9cada50fdc5536b6460f14be
Robin Genz <mail@robingenz.dev>
2020-12-02 21:11:31 +0100

    test(semester-select): add tests

--- src/app/modules/exam-results/components/semester-select/semester-select.component.spec.ts
+++ src/app/modules/exam-results/components/semester-select/semester-select.component.spec.ts
@@ -1,3 +1,4 @@
+import { ChangeDetectionStrategy } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { SelectChangeEventDetail } from '@ionic/core';
@@ -7,19 +8,29 @@ import { SemesterSelectComponent } from './semester-select.component';
 describe('SemesterSelectComponent', () => {
   let component: SemesterSelectComponent;
   let fixture: ComponentFixture<SemesterSelectComponent>;
+  let element: HTMLElement;
 
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
         declarations: [SemesterSelectComponent],
         imports: [IonicModule.forRoot()],
-      }).compileComponents();
+      })
+        .overrideComponent(SemesterSelectComponent, {
+          set: { changeDetection: ChangeDetectionStrategy.Default },
+        })
+        .compileComponents();
 
       fixture = TestBed.createComponent(SemesterSelectComponent);
       component = fixture.componentInstance;
+      element = fixture.nativeElement;
       fixture.detectChanges();
     }),
   );
+
+  afterEach(() => {
+    fixture.nativeElement.remove();
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -33,5 +44,22 @@ describe('SemesterSelectComponent', () => {
       }),
     );
     expect(component.selectSemester.emit).toHaveBeenCalledWith({ id: '1', displayName: 'Test' });
+  });
+
+  it('should not display any html elements', () => {
+    component.semesterList = [];
+    fixture.detectChanges();
+    const ionItemElm = element.querySelector('ion-item');
+    expect(ionItemElm).toBeNull();
+  });
+
+  it('should display the select options', () => {
+    component.semesterList = [
+      { id: '1', displayName: '1' },
+      { id: '2', displayName: '2' },
+    ];
+    fixture.detectChanges();
+    const list = element.querySelectorAll('ion-select-option');
+    expect(list.length).toBe(2);
   });
 });
