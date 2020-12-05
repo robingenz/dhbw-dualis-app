0001f4731da86ea34a324ff6d553fb1ea3a46097
Robin Genz <mail@robingenz.dev>
2020-12-05 19:25:57 +0100

    test: add component tests (#68)
    * test: add test to `SemesterSelectComponent`
    
    * test: clean up native elements
    
    * test(semester-select): add tests
    
    * test: add tests
    
    * test: add tests
    
    * test: fix tests

--- src/app/modules/exam-results/components/semester-select/semester-select.component.spec.ts
+++ src/app/modules/exam-results/components/semester-select/semester-select.component.spec.ts
@@ -1,26 +1,72 @@
+import { ChangeDetectionStrategy } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { SelectChangeEventDetail } from '@ionic/core';
+import { SemesterListItem } from '../../interfaces';
 import { SemesterSelectComponent } from './semester-select.component';
 
 describe('SemesterSelectComponent', () => {
   let component: SemesterSelectComponent;
   let fixture: ComponentFixture<SemesterSelectComponent>;
+  let element: HTMLElement;
 
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
         declarations: [SemesterSelectComponent],
         imports: [IonicModule.forRoot()],
-      }).compileComponents();
+      })
+        .overrideComponent(SemesterSelectComponent, {
+          set: { changeDetection: ChangeDetectionStrategy.Default },
+        })
+        .compileComponents();
 
       fixture = TestBed.createComponent(SemesterSelectComponent);
       component = fixture.componentInstance;
+      element = fixture.nativeElement;
       fixture.detectChanges();
     }),
   );
 
+  afterEach(() => {
+    fixture.nativeElement.remove();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display ion-item', () => {
+    component.semesterList = [{ id: '', displayName: '' }];
+    fixture.detectChanges();
+    const ionItemElm = element.querySelector('ion-item');
+    expect(ionItemElm).toBeTruthy();
+  });
+
+  it('should not display ion-item', () => {
+    component.semesterList = [];
+    fixture.detectChanges();
+    const ionItemElm = element.querySelector('ion-item');
+    expect(ionItemElm).toBeNull();
+  });
+
+  it('should emit a event on semester select', () => {
+    spyOn(component.selectSemester, 'emit');
+    component.onSemesterSelect(
+      new CustomEvent<SelectChangeEventDetail<SemesterListItem>>('', {
+        detail: { value: { id: '1', displayName: 'Test' } },
+      }),
+    );
+    expect(component.selectSemester.emit).toHaveBeenCalledWith({ id: '1', displayName: 'Test' });
+  });
+
+  it('should display the select options', () => {
+    component.semesterList = [
+      { id: '1', displayName: '1' },
+      { id: '2', displayName: '2' },
+    ];
+    fixture.detectChanges();
+    const list = element.querySelectorAll('ion-select-option');
+    expect(list.length).toBe(2);
   });
 });
--- src/app/modules/exam-results/components/unit-card/unit-card.component.spec.ts
+++ src/app/modules/exam-results/components/unit-card/unit-card.component.spec.ts
@@ -1,26 +1,59 @@
+import { ChangeDetectionStrategy } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { EmptyStringPipe } from '../../pipes';
 import { UnitCardComponent } from './unit-card.component';
 
 describe('UnitCardComponent', () => {
   let component: UnitCardComponent;
   let fixture: ComponentFixture<UnitCardComponent>;
+  let element: HTMLElement;
 
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
-        declarations: [UnitCardComponent],
+        declarations: [UnitCardComponent, EmptyStringPipe],
         imports: [IonicModule.forRoot()],
-      }).compileComponents();
+      })
+        .overrideComponent(UnitCardComponent, {
+          set: { changeDetection: ChangeDetectionStrategy.Default },
+        })
+        .compileComponents();
 
       fixture = TestBed.createComponent(UnitCardComponent);
       component = fixture.componentInstance;
+      element = fixture.nativeElement;
       fixture.detectChanges();
     }),
   );
 
+  afterEach(() => {
+    fixture.nativeElement.remove();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display ion-card', () => {
+    component.unit = {
+      id: '111222111222111',
+      displayName: 'Wissenschaftliches Arbeiten',
+      no: 'WWI_100',
+      status: 'bestanden',
+      credits: '5,0',
+      finalGrade: 'b',
+      exams: undefined,
+    };
+    fixture.detectChanges();
+    const ionItemElm = element.querySelector('ion-card');
+    expect(ionItemElm).toBeTruthy();
+  });
+
+  it('should not display ion-card', () => {
+    component.unit = undefined;
+    fixture.detectChanges();
+    const ionItemElm = element.querySelector('ion-card');
+    expect(ionItemElm).toBeNull();
   });
 });
