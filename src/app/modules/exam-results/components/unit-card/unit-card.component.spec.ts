78b0ddd78233fe396d2d48799390794436a37378
Robin Genz <mail@robingenz.dev>
2020-12-05 17:55:03 +0100

    test: add tests

--- src/app/modules/exam-results/components/unit-card/unit-card.component.spec.ts
+++ src/app/modules/exam-results/components/unit-card/unit-card.component.spec.ts
@@ -1,3 +1,4 @@
+import { ChangeDetectionStrategy } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { UnitCardComponent } from './unit-card.component';
@@ -5,13 +6,18 @@ import { UnitCardComponent } from './unit-card.component';
 describe('UnitCardComponent', () => {
   let component: UnitCardComponent;
   let fixture: ComponentFixture<UnitCardComponent>;
+  let element: HTMLElement;
 
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
         declarations: [UnitCardComponent],
         imports: [IonicModule.forRoot()],
-      }).compileComponents();
+      })
+        .overrideComponent(UnitCardComponent, {
+          set: { changeDetection: ChangeDetectionStrategy.Default },
+        })
+        .compileComponents();
 
       fixture = TestBed.createComponent(UnitCardComponent);
       component = fixture.componentInstance;
@@ -25,5 +31,27 @@ describe('UnitCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display ion-card', () => {
+    component.unit = {
+      id: '111222111222111',
+      displayName: 'Wissenschaftliches Arbeiten',
+      no: 'WWI_100',
+      status: 'bestanden',
+      credits: '5,0',
+      finalGrade: 'b',
+      exams: undefined,
+    };
+    fixture.detectChanges();
+    const ionItemElm = element.querySelector('ion-card');
+    expect(ionItemElm).toBeTruthy();
+  });
+
+  it('should not display ion-card', () => {
+    component.unit = undefined;
+    fixture.detectChanges();
+    const ionItemElm = element.querySelector('ion-card');
+    expect(ionItemElm).toBeNull();
   });
 });
