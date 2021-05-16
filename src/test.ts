dc7932117d4b603734cabc56abef5ffdf9c34c5a
Robin Genz <mail@robingenz.dev>
2021-05-16 13:05:46 +0200

    chore(deps): update to Angular 12 (#176)

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 /* tslint:disable:ordered-imports */
-import 'zone.js/dist/zone-testing'; // Must be the FIRST import!
+import 'zone.js/testing'; // Must be the FIRST import!
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import { improveChangeDetection, muteIonicReInitializeWarning } from '@tests/test-setup';
